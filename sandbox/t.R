692f7a65a4b74f317872e323ee19d2af6b1faf5e
sdirkse <sdirkse>
2011-06-24 04:39:50 +0000

    compute confidence intervals for residual errors

--- sandbox/t.R
+++ sandbox/t.R
@@ -13,14 +13,16 @@ d <- data.frame(y=data$val[,1],x=data$val[,2])
 
 f <- y ~ x
 res <- lm(f, data = d)
+est <- as.array(coef(res))
+se <- as.array(summary(res)$coefficients[,'Std. Error'])
+df <- res$df.residual
 
-ev <- as.array(coef(res))
-est <- list(name='estimate', type='parameter', form='full',
-            ts='Estimated coefficients', val=ev, uels=p)
+estlst <- list(name='estimate', type='parameter', form='full',
+               ts='Estimated coefficients', val=est, uels=p)
 
 selst <- list(name='se', type='parameter', form='full',
             ts='Standard errors',
-            val=as.array(summary(res)$coefficients[,'Std. Error']), uels=p)
+            val=se, uels=p)
 
 tlst <- list(name='tval', type='parameter', form='full',
              ts='t values',
@@ -31,8 +33,7 @@ plst <- list(name='pval', type='parameter', form='full',
             val=as.array(summary(res)$coefficients[,'Pr(>|t|)']), uels=p)
 
 dflst <- list(name='df', type='parameter', form='full', dim=0,
-              ts='Degrees of freedom',
-              val=summary(res)$df[2])
+              ts='Degrees of freedom', val=df)
 
 sigmalst <- list(name='sigma', type='parameter', form='full', dim=0,
                  ts='Standard error',
@@ -50,10 +51,25 @@ fittedlst <- list(name='fitted', type='parameter', form='full',
              ts='fitted values for dependent variable',
              val=as.array(res$fitted.values), uels=i)
 
-wgdx (fnSol, est, selst, tlst, plst, dflst, sigmalst, r2lst,
-      residualslst, fittedlst)
+# set of confidence intervals to write
+cnf <- c("90%"=.9, "95%"=.95, "97.5%"=.975, "99%"=.99)
+confs <- list(attr(cnf,"names"))
+ncnf  <- length(cnf)
+bnds  <- list(c("LO", "UP"))
+nbnds <- length(bnds[[1]])
+confint <- array(0.5,dim=c(ncnf,length(p[[1]]),nbnds))
+for (k in 1:ncnf) {
+  tstar <- qt((1+cnf[k])/2,df=df)
+  confint[k,,1] <- est - tstar * se
+  confint[k,,2] <- est + tstar * se
+}
+conflst <- list(name='confint', type='parameter', form='full',
+             ts='confidence intervals',
+             val=confint, uels=c(confs,p,bnds))
+
+wgdx (fnSol, estlst, selst, tlst, plst, dflst, sigmalst, r2lst,
+      residualslst, fittedlst, conflst)
 # still to do:
-#  confint: must compute??
 #  covar: ???
 #  resvar
 #  rss
@@ -70,7 +86,7 @@ if (0 != rc) {
   print ("tight gdxdiff call succeeded")
 }
 
-rc <- system (paste("gdxdiff ls.gdx",fnSol,"releps=1e-5 eps=1e-30 id=tval,pval"))
+rc <- system (paste("gdxdiff ls.gdx",fnSol,"releps=1e-5 eps=1e-30 id=tval,pval,confint"))
 if (0 != rc) {
   stop(paste("Bad return from loose gdxdiff: wanted 0, got",rc))
 } else {
