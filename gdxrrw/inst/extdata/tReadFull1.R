bd8d8a67861f897391db4cb7f3a9d77121938dc5
sdirkse <sdirkse>
2011-06-30 04:24:34 +0000

    update and augment tests

--- gdxrrw/inst/extdata/tReadFull1.R
+++ gdxrrw/inst/extdata/tReadFull1.R
@@ -3,17 +3,17 @@
 
 # compare the set s to the universe, return TRUE if the same, FALSE o/w
 chkUni <- function(uni,s) {
-  if (! is.vector(s))     return (FALSE);
-  if (! is.vector(uni))   return (FALSE);
+  if (! is.vector(s))     return (FALSE)
+  if (! is.vector(uni))   return (FALSE)
   n <- length(uni)    
-  if (n != length(s))     return (FALSE);
+  if (n != length(s))     return (FALSE)
   for (k in c(1:n)) {
     if (uni[k] != s[k]) {
-      print ("chkUni: UEL with index ", k, " is wrong");
-      return (FALSE);
+      print ("chkUni: UEL with index ", k, " is wrong")
+      return (FALSE)
     }
   }
-  return (TRUE);
+  return (TRUE)
 }
 
 
@@ -23,9 +23,9 @@ tryCatch({
   print ("using the transport data as the input")
   rgdx('?')
 
-  u <- rgdx('trnsport');
+  u <- rgdx('trnsport')
   if (!is.list(u))
-    stop ("Expected rgdx output to be in list form");
+    stop ("Expected rgdx output to be in list form")
 
   lst <- list(name='i',form='full')
   i <- rgdx('trnsport',lst)
@@ -39,17 +39,15 @@ tryCatch({
     stop ("Expected i$form to be 'full', got ", i$form)
   if (length(i$uels) != i$dim)
     stop ("Expected length of i$uels to be equal i$dim, got ", length(i$uels))
-  for (d in dim(i$uels)) {
-    if (! chkUni(u$uels,i$uels[[d]])) {
-      stop ("Expected universe in dim ", d, " of i$uels");
-    }
+  if (! chkUni(u$uels,i$uels[[1]])) {
+    stop ("Expected universe in dim 1 of i$uels")
   }
-  iv <- matrix(c(1,1,0,0,0), c(5,1));
+  iv <- matrix(c(1,1,0,0,0), c(5,1))
   dd <- iv == i$val
   if (! prod(dd)) {
-    stop ("Bad data in i$val: ", dd);
+    stop ("Bad data in i$val: ", dd)
   }
-  print ("Done reading set i");
+  print ("Done reading set i")
 
   lst <- list(name='j',form='full')
   j <- rgdx('trnsport',lst)
@@ -63,19 +61,114 @@ tryCatch({
     stop ("Expected j$form to be 'full', got ", j$form)
   if (length(j$uels) != j$dim)
     stop ("Expected length of j$uels to be equal j$dim, got ", length(j$uels))
-  for (d in dim(j$uels)) {
-    if (! chkUni(u$uels,j$uels[[d]])) {
-      stop ("Expected universe in dim ", d, " of j$uels");
-    }
+  if (! chkUni(u$uels,j$uels[[1]])) {
+    stop ("Expected universe in dim 1 of j$uels")
   }
-  jv <- matrix(c(0,0,1,1,1), c(5,1));
+  jv <- matrix(c(0,0,1,1,1), c(5,1))
   dd <- jv == j$val
   if (! prod(dd)) {
-    stop ("Bad data in j$val: ", dd);
+    stop ("Bad data in j$val: ", dd)
   }
-  print ("Done reading set j");
+  print ("Done reading set j")
 
-  print ("Successfully completed tests");
+  lst <- list(name='f',form='full')
+  f <- rgdx('trnsport',lst)
+  if (f$name != 'f')
+    stop ("Expected f$name to be 'f', got ", f$name)
+  if (f$type != "parameter")
+    stop ("Expected f$type to be 'parameter', got ", f$type)
+  if (f$dim != 0)
+    stop ("Expected f$dim to be 0, got ", f$dim)
+  if (f$form != "full")
+    stop ("Expected f$form to be 'full', got ", f$form)
+  if (! is.vector(f$val))
+    stop ("Expected f$val to be a vector")
+  dd <- 90 == f$val
+  if (! prod(dd)) {
+    stop ("Bad data in f$val: ", dd)
+  }
+  if (length(f$uels) != 0)
+    stop ("Expected f$uels to be empty")
+  print ("Done reading scalar f")
+
+  lst <- list(name='a',form='full')
+  a <- rgdx('trnsport',lst)
+  if (a$name != 'a')
+    stop ("Expected a$name to be 'a', got ", a$name)
+  if (a$type != "parameter")
+    stop ("Expected a$type to be 'parameter', got ", a$type)
+  if (a$dim != 1)
+    stop ("Expected a$dim to be 1, got ", a$dim)
+  if (a$form != "full")
+    stop ("Expected a$form to be 'full', got ", a$form)
+  if (! is.matrix(a$val))
+    stop ("Expected a$val to be a matrix")
+  av <- matrix(c(350,600,0,0,0), c(5,1))
+  dd <- av == a$val
+  if (! prod(dd)) {
+    stop ("Bad data in a$val: ", dd)
+  }
+  if (length(a$uels) != 1)
+    stop ("Expected length of a$uels to be 1, got ", length(a$uels))
+  if (! chkUni(u$uels,a$uels[[1]])) {
+    stop ("Expected universe in dim 1 of a$uels")
+  }
+  print ("Done reading parameter a")
+
+  lst <- list(name='b',form='full')
+  b <- rgdx('trnsport',lst)
+  if (b$name != 'b')
+    stop ("Expected b$name to be 'b', got ", b$name)
+  if (b$type != "parameter")
+    stop ("Expected b$type to be 'parameter', got ", b$type)
+  if (b$dim != 1)
+    stop ("Expected b$dim to be 1, got ", b$dim)
+  if (b$form != "full")
+    stop ("Expected b$form to be 'full', got ", b$form)
+  if (! is.matrix(b$val))
+    stop ("Expected b$val to be a matrix")
+  bv <- matrix(c(0,0,325,300,275), c(5,1))
+  dd <- bv == b$val
+  if (! prod(dd)) {
+    stop ("Bad data in b$val: ", dd)
+  }
+  if (length(b$uels) != 1)
+    stop ("Expected length of b$uels to be 1, got ", length(b$uels))
+  if (! chkUni(u$uels,b$uels[[1]])) {
+    stop ("Expected universe in dim 1 of b$uels")
+  }
+  print ("Done reading parameter b")
+
+  lst <- list(name='c',form='full')
+  c <- rgdx('trnsport',lst)
+  if (c$name != 'c')
+    stop ("Expected c$name to be 'c', got ", c$name)
+  if (c$type != "parameter")
+    stop ("Expected c$type to be 'parameter', got ", c$type)
+  if (c$dim != 2)
+    stop ("Expected c$dim to be 2, got ", c$dim)
+  if (c$form != "full")
+    stop ("Expected c$form to be 'full', got ", c$form)
+  if (length(dim(c$val)) != 2)
+    stop ("Expected c$val to be a matrix")
+  dv <- matrix(0,nrow=5,ncol=5)
+  dvp <- matrix(c(2.5, 1.7, 1.8,
+                  2.5, 1.8, 1.4),  c(2,3), byrow=TRUE)
+  dv[(1:2),(3:5)] <- dvp
+  cv <- dv * 90 / 1000
+  dd <- abs(cv - c$val) < 1e-13
+  if (! prod(dd)) {
+    stop ("Bad data in c$val: ", dd)
+  }
+  for (k in c(1:length(c$uels))) {
+    if (! chkUni(u$uels,c$uels[[k]])) {
+      stop ("Expected universe in dim ", k, " of c$uels")
+    }
+  }
+  print ("Done reading parameter c")
+
+
+  print ("Successfully completed tests")
   return (TRUE)
 }
 
--- gdxrrw/inst/extdata/tReadSparse1.R
+++ gdxrrw/inst/extdata/tReadSparse1.R
@@ -3,295 +3,295 @@
 # This test does near-exhaustive checks on the returned structure
 
 tryCatch({
-  print ("Test rgdx with defaults (form='sparse' and no filter)");
-  print ("using the transport data as input");
-  rgdx('?');
+  print ("Test rgdx with defaults (form='sparse' and no filter)")
+  print ("using the transport data as input")
+  rgdx('?')
 
-  u <- rgdx('trnsport');
+  u <- rgdx('trnsport')
   if (!is.list(u))
-    stop ("Expected rgdx output to be in list form");
+    stop ("Expected rgdx output to be in list form")
   if (length(u) != 6)
-    stop ("Expected the list returned to have 6 components");
+    stop ("Expected the list returned to have 6 components")
   if (typeof(u$name) != "NULL")
-    stop ("Expected u$name to be NULL");
+    stop ("Expected u$name to be NULL")
   if (typeof(u$type) != "NULL")
-    stop ("Expected u$type to be NULL");
+    stop ("Expected u$type to be NULL")
   if (typeof(u$dim) != "NULL")
-    stop ("Expected u$dim to be NULL");
+    stop ("Expected u$dim to be NULL")
   if (typeof(u$val) != "NULL")
-    stop ("Expected u$val to be NULL");
+    stop ("Expected u$val to be NULL")
   if (typeof(u$form) != "NULL")
-    stop ("Expected u$form to be NULL");
-  allUels <- c('seattle', "san-diego", "new-york", "chicago", "topeka");
+    stop ("Expected u$form to be NULL")
+  allUels <- c('seattle', "san-diego", "new-york", "chicago", "topeka")
   for (k in 1:length(u$uels)) {
     if (u$uels[k] != allUels[k]) {
-      stop ("UEL with index ", k, " is wrong");
+      stop ("UEL with index ", k, " is wrong")
     }
   }
 
-  lst <- list(name='i');
-  i <- rgdx('trnsport',lst);
+  lst <- list(name='i')
+  i <- rgdx('trnsport',lst)
   if (!is.list(i))
-    stop ("Expected rgdx output to be in list form");
+    stop ("Expected rgdx output to be in list form")
   if (length(i) != 6)
-    stop ("Expected the list returned to have 6 components");
+    stop ("Expected the list returned to have 6 components")
   if (i$name != 'i')
-    stop ("Expected i$name to be 'i', got ", i$name);
+    stop ("Expected i$name to be 'i', got ", i$name)
   if (i$type != "set")
-    stop ("Expected i$type to be 'set', got ", i$type);
+    stop ("Expected i$type to be 'set', got ", i$type)
   if (i$dim != 1)
-    stop ("Expected i$dim to be 1, got ", i$dim);
+    stop ("Expected i$dim to be 1, got ", i$dim)
   if (i$form != "sparse")
-    stop ("Expected i$form to be 'sparse', got ", i$form);
+    stop ("Expected i$form to be 'sparse', got ", i$form)
   if (length(dim(i$val)) != 2)
-    stop ("Expected i$val to be a matrix");
+    stop ("Expected i$val to be a matrix")
   if (dim(i$val)[1] != 2)
-    stop ("Expected dim(i$val)[1] to be 2");
+    stop ("Expected dim(i$val)[1] to be 2")
   if (dim(i$val)[2] != 1)
-    stop ("Expected dim(i$val)[2] to be 1");
-  iv <- matrix(c(1:2), c(2,1));
+    stop ("Expected dim(i$val)[2] to be 1")
+  iv <- matrix(c(1:2), c(2,1))
   for (k in dim(i$val)[1] ) {
     if (i$val[k,1] != iv[k,1]) {
-      stop ("Bad data in i$val, row ", k);
+      stop ("Bad data in i$val, row ", k)
     }
   }
-  lst <- list(name='j');
-  j <- rgdx('trnsport',lst);
+  lst <- list(name='j')
+  j <- rgdx('trnsport',lst)
   if (!is.list(j))
-    stop ("Expected rgdx output to be in list form");
+    stop ("Expected rgdx output to be in list form")
   if (length(j) != 6)
-    stop ("Expected the list returned to have 6 components");
+    stop ("Expected the list returned to have 6 components")
   if (j$name != 'j')
-    stop ("Expected j$name to be 'j', got ", j$name);
+    stop ("Expected j$name to be 'j', got ", j$name)
   if (j$type != "set")
-    stop ("Expected j$type to be 'set', got ", j$type);
+    stop ("Expected j$type to be 'set', got ", j$type)
   if (j$dim != 1)
-    stop ("Expected j$dim to be 1, got ", j$dim);
+    stop ("Expected j$dim to be 1, got ", j$dim)
   if (j$form != "sparse")
-    stop ("Expected j$form to be 'sparse', got ", j$form);
+    stop ("Expected j$form to be 'sparse', got ", j$form)
   if (length(dim(j$val)) != 2)
-    stop ("Expected j$val to be a matrix");
+    stop ("Expected j$val to be a matrix")
   if (dim(j$val)[1] != 3)
-    stop ("Expected dim(j$val)[1] to be 3");
+    stop ("Expected dim(j$val)[1] to be 3")
   if (dim(j$val)[2] != 1)
-    stop ("Expected dim(j$val)[2] to be 1");
-  jv <- matrix(c(3:5), c(3,1));
+    stop ("Expected dim(j$val)[2] to be 1")
+  jv <- matrix(c(3:5), c(3,1))
   for (k in dim(j$val)[1] ) {
     if (j$val[k,1] != jv[k,1]) {
-      stop ("Bad data in j$val, row ", k);
+      stop ("Bad data in j$val, row ", k)
     }
   }
 
-  lst <- list(name='f');
-  f <- rgdx('trnsport',lst);
+  lst <- list(name='f')
+  f <- rgdx('trnsport',lst)
   if (!is.list(f))
-    stop ("Expected rgdx output to be in list form");
+    stop ("Expected rgdx output to be in list form")
   if (length(f) != 6)
-    stop ("Expected the list returned to have 6 components");
+    stop ("Expected the list returned to have 6 components")
   if (f$name != 'f')
-    stop ("Expected f$name to be 'f', got ", f$name);
+    stop ("Expected f$name to be 'f', got ", f$name)
   if (f$type != "parameter")
-    stop ("Expected f$type to be 'parameter', got ", f$type);
+    stop ("Expected f$type to be 'parameter', got ", f$type)
   if (f$dim != 0)
-    stop ("Expected f$dim to be 0, got ", f$dim);
+    stop ("Expected f$dim to be 0, got ", f$dim)
   if (f$form != "sparse")
-    stop ("Expected f$form to be 'sparse', got ", f$form);
+    stop ("Expected f$form to be 'sparse', got ", f$form)
   if (length(dim(f$val)) != 2)
-    stop ("Expected f$val to be a matrix");
+    stop ("Expected f$val to be a matrix")
   if (dim(f$val)[1] != 1)
-    stop ("Expected dim(f$val)[1] to be 1");
+    stop ("Expected dim(f$val)[1] to be 1")
   if (dim(f$val)[2] != 1)
-    stop ("Expected dim(f$val)[2] to be 1");
+    stop ("Expected dim(f$val)[2] to be 1")
   if (f$val[1,1] != 90)
-    stop ("Bad data in f$val, row 1");
+    stop ("Bad data in f$val, row 1")
 
-  lst <- list(name='a');
-  a <- rgdx('trnsport',lst);
+  lst <- list(name='a')
+  a <- rgdx('trnsport',lst)
   if (!is.list(a))
-    stop ("Expected rgdx output to be in list form");
+    stop ("Expected rgdx output to be in list form")
   if (length(a) != 6)
-    stop ("Expected the list returned to have 6 components");
+    stop ("Expected the list returned to have 6 components")
   if (a$name != 'a')
-    stop ("Expected a$name to be 'a', got ", a$name);
+    stop ("Expected a$name to be 'a', got ", a$name)
   if (a$type != "parameter")
-    stop ("Expected a$type to be 'parameter', got ", a$type);
+    stop ("Expected a$type to be 'parameter', got ", a$type)
   if (a$dim != 1)
-    stop ("Expected a$dim to be 1, got ", a$dim);
+    stop ("Expected a$dim to be 1, got ", a$dim)
   if (a$form != "sparse")
-    stop ("Expected a$form to be 'sparse', got ", a$form);
+    stop ("Expected a$form to be 'sparse', got ", a$form)
   if (length(dim(a$val)) != 2)
-    stop ("Expected a$val to be a matrix");
+    stop ("Expected a$val to be a matrix")
   if (dim(a$val)[1] != 2)
-    stop ("Expected dim(a$val)[1] to be 2");
+    stop ("Expected dim(a$val)[1] to be 2")
   if (dim(a$val)[2] != 2)
-    stop ("Expected dim(a$val)[2] to be 2");
-  av <- matrix(c(1,2,350,600), c(2,2));
+    stop ("Expected dim(a$val)[2] to be 2")
+  av <- matrix(c(1,2,350,600), c(2,2))
   for (ki in dim(a$val)[1] ) {
     for (kj in dim(a$val)[2] ) {
       if (a$val[ki,kj] != av[ki,kj]) {
-        stop ("Bad data in a$val, row ", ki, " col ", kj);
+        stop ("Bad data in a$val, row ", ki, " col ", kj)
       }
     }
   }
 
-  lst <- list(name='b');
-  b <- rgdx('trnsport',lst);
+  lst <- list(name='b')
+  b <- rgdx('trnsport',lst)
   if (!is.list(b))
-    stop ("Expected rgdx output to be in list form");
+    stop ("Expected rgdx output to be in list form")
   if (length(b) != 6)
-    stop ("Expected the list returned to have 6 components");
+    stop ("Expected the list returned to have 6 components")
   if (b$name != 'b')
-    stop ("Expected b$name to be 'b', got ", b$name);
+    stop ("Expected b$name to be 'b', got ", b$name)
   if (b$type != "parameter")
-    stop ("Expected b$type to be 'parameter', got ", b$type);
+    stop ("Expected b$type to be 'parameter', got ", b$type)
   if (b$dim != 1)
-    stop ("Expected b$dim to be 1, got ", b$dim);
+    stop ("Expected b$dim to be 1, got ", b$dim)
   if (b$form != "sparse")
-    stop ("Expected b$form to be 'sparse', got ", b$form);
+    stop ("Expected b$form to be 'sparse', got ", b$form)
   if (length(dim(b$val)) != 2)
-    stop ("Expected b$val to be a matrix");
+    stop ("Expected b$val to be a matrix")
   if (dim(b$val)[1] != 3)
-    stop ("Expected dim(b$val)[1] to be 3");
+    stop ("Expected dim(b$val)[1] to be 3")
   if (dim(b$val)[2] != 2)
-    stop ("Expected dim(b$val)[2] to be 2");
-  bv <- matrix(c(3:5,325,300,275), c(3,2));
+    stop ("Expected dim(b$val)[2] to be 2")
+  bv <- matrix(c(3:5,325,300,275), c(3,2))
   for (ki in dim(b$val)[1] ) {
     for (kj in dim(b$val)[2] ) {
       if (b$val[ki,kj] != bv[ki,kj]) {
-        stop ("Bad data in b$val, row ", ki, " col ", kj);
+        stop ("Bad data in b$val, row ", ki, " col ", kj)
       }
     }
   }
 
-  lst <- list(name='c');
-  c <- rgdx('trnsport',lst);
+  lst <- list(name='c')
+  c <- rgdx('trnsport',lst)
   if (!is.list(c))
-    stop ("Expected rgdx output to be in list form");
+    stop ("Expected rgdx output to be in list form")
   if (length(c) != 6)
-    stop ("Expected the list returned to have 6 components");
+    stop ("Expected the list returned to have 6 components")
   if (c$name != 'c')
-    stop ("Expected c$name to be 'c', got ", c$name);
+    stop ("Expected c$name to be 'c', got ", c$name)
   if (c$type != "parameter")
-    stop ("Expected c$type to be 'parameter', got ", c$type);
+    stop ("Expected c$type to be 'parameter', got ", c$type)
   if (c$dim != 2)
-    stop ("Expected c$dim to be 2, got ", c$dim);
+    stop ("Expected c$dim to be 2, got ", c$dim)
   if (c$form != "sparse")
-    stop ("Expected c$form to be 'sparse', got ", c$form);
+    stop ("Expected c$form to be 'sparse', got ", c$form)
   if (length(dim(c$val)) != 2)
-    stop ("Expected c$val to be a matrix");
+    stop ("Expected c$val to be a matrix")
   if (dim(c$val)[1] != 6)
-    stop ("Expected dim(c$val)[1] to be 6");
+    stop ("Expected dim(c$val)[1] to be 6")
   if (dim(c$val)[2] != 3)
-    stop ("Expected dim(c$val)[2] to be 3");
+    stop ("Expected dim(c$val)[2] to be 3")
   cv <- matrix(c(1,3,.225,
                  1,4,.153,
                  1,5,.162,
                  2,3,.225,
                  2,4,.162,
-                 2,5,.126) , c(6,3), byrow=TRUE);
+                 2,5,.126) , c(6,3), byrow=TRUE)
   for (ki in dim(c$val)[1] ) {
     for (kj in dim(c$val)[2] ) {
       if (c$val[ki,kj] != cv[ki,kj]) {
-        stop ("Bad data in c$val, row ", ki, " col ", kj);
+        stop ("Bad data in c$val, row ", ki, " col ", kj)
       }
     }
   }
 
-  lst <- list(name='d');
-  d <- rgdx('trnsport',lst);
+  lst <- list(name='d')
+  d <- rgdx('trnsport',lst)
   if (!is.list(d))
-    stop ("Expected rgdx output to be in list form");
+    stop ("Expected rgdx output to be in list form")
   if (length(d) != 6)
-    stop ("Expected the list returned to have 6 components");
+    stop ("Expected the list returned to have 6 components")
   if (d$name != 'd')
-    stop ("Expected d$name to be 'd', got ", d$name);
+    stop ("Expected d$name to be 'd', got ", d$name)
   if (d$type != "parameter")
-    stop ("Expected d$type to be 'parameter', got ", d$type);
+    stop ("Expected d$type to be 'parameter', got ", d$type)
   if (d$dim != 2)
-    stop ("Expected d$dim to be 2, got ", d$dim);
+    stop ("Expected d$dim to be 2, got ", d$dim)
   if (d$form != "sparse")
-    stop ("Expected d$form to be 'sparse', got ", d$form);
+    stop ("Expected d$form to be 'sparse', got ", d$form)
   if (length(dim(d$val)) != 2)
-    stop ("Expected d$val to be a matrix");
+    stop ("Expected d$val to be a matrix")
   if (dim(d$val)[1] != 6)
-    stop ("Expected dim(d$val)[1] to be 6");
+    stop ("Expected dim(d$val)[1] to be 6")
   if (dim(d$val)[2] != 3)
-    stop ("Expected dim(d$val)[2] to be 3");
+    stop ("Expected dim(d$val)[2] to be 3")
   dv <- matrix(c(1,3,2.5,
                  1,4,1.7,
                  1,5,1.8,
                  2,3,2.5,
                  2,4,1.8,
-                 2,5,1.4) , c(6,3), byrow=TRUE);
+                 2,5,1.4) , c(6,3), byrow=TRUE)
   for (ki in dim(d$val)[1] ) {
     for (kj in dim(d$val)[2] ) {
       if (d$val[ki,kj] != dv[ki,kj]) {
-        stop ("Bad data in d$val, row ", ki, " col ", kj);
+        stop ("Bad data in d$val, row ", ki, " col ", kj)
       }
     }
   }
 
-  lst <- list(name='x');
-  x <- rgdx('trnsport',lst);
+  lst <- list(name='x')
+  x <- rgdx('trnsport',lst)
   if (!is.list(x))
-    stop ("Expected rgdx output to be in list form");
+    stop ("Expected rgdx output to be in list form")
   if (length(x) != 7)
-    stop ("Expected the list returned to have 7 components");
+    stop ("Expected the list returned to have 7 components")
   if (x$name != 'x')
-    stop ("Expected x$name to be 'x', got ", x$name);
+    stop ("Expected x$name to be 'x', got ", x$name)
   if (x$type != "variable")
-    stop ("Expected x$type to be 'variable', got ", x$type);
+    stop ("Expected x$type to be 'variable', got ", x$type)
   if (x$dim != 2)
-    stop ("Expected x$dim to be 2, got ", x$dim);
+    stop ("Expected x$dim to be 2, got ", x$dim)
   if (x$form != "sparse")
-    stop ("Expected x$form to be 'sparse', got ", x$form);
+    stop ("Expected x$form to be 'sparse', got ", x$form)
   if (x$field != "l")
-    stop ("Expected x$field to be 'l', got ", x$field);
+    stop ("Expected x$field to be 'l', got ", x$field)
   if (length(dim(x$val)) != 2)
-    stop ("Expected x$val to be a matrix");
+    stop ("Expected x$val to be a matrix")
   if (dim(x$val)[1] != 4)
-    stop ("Expected dim(x$val)[1] to be 4");
+    stop ("Expected dim(x$val)[1] to be 4")
   if (dim(x$val)[2] != 3)
-    stop ("Expected dim(x$val)[2] to be 3");
+    stop ("Expected dim(x$val)[2] to be 3")
   xlv <- matrix(c(1,3, 50,
                   1,4,300,
                   2,3,275,
-                  2,5,275) , c(4,3), byrow=TRUE);
+                  2,5,275) , c(4,3), byrow=TRUE)
   for (ki in dim(x$val)[1] ) {
     for (kj in dim(x$val)[2] ) {
       if (x$val[ki,kj] != xlv[ki,kj]) {
-        stop ("Bad data in x$val, row ", ki, " col ", kj);
+        stop ("Bad data in x$val, row ", ki, " col ", kj)
       }
     }
   }
 
-  lst <- list(name='z');
-  z <- rgdx('trnsport',lst);
+  lst <- list(name='z')
+  z <- rgdx('trnsport',lst)
   if (!is.list(z))
-    stop ("Expected rgdx output to be in list form");
+    stop ("Expected rgdx output to be in list form")
   if (length(z) != 7)
-    stop ("Expected the list returned to have 7 components");
+    stop ("Expected the list returned to have 7 components")
   if (z$name != 'z')
-    stop ("Expected z$name to be 'z', got ", z$name);
+    stop ("Expected z$name to be 'z', got ", z$name)
   if (z$type != "variable")
-    stop ("Expected z$type to be 'variable', got ", z$type);
+    stop ("Expected z$type to be 'variable', got ", z$type)
   if (z$dim != 0)
-    stop ("Expected z$dim to be 0, got ", z$dim);
+    stop ("Expected z$dim to be 0, got ", z$dim)
   if (z$form != "sparse")
-    stop ("Expected z$form to be 'sparse', got ", z$form);
+    stop ("Expected z$form to be 'sparse', got ", z$form)
   if (z$field != "l")
-    stop ("Expected z$field to be 'l', got ", z$field);
+    stop ("Expected z$field to be 'l', got ", z$field)
   if (length(dim(z$val)) != 2)
-    stop ("Expected z$val to be a matrix");
+    stop ("Expected z$val to be a matrix")
   if (dim(z$val)[1] != 1)
-    stop ("Expected dim(z$val)[1] to be 1");
+    stop ("Expected dim(z$val)[1] to be 1")
   if (dim(z$val)[2] != 1)
-    stop ("Expected dim(z$val)[2] to be 1");
+    stop ("Expected dim(z$val)[2] to be 1")
   if (z$val[1,1] != 153.675)
-    stop ("Bad data in z$val");
+    stop ("Bad data in z$val")
 
 
-  print ("Successfully completed tests");
+  print ("Successfully completed tests")
   return (TRUE)
 }
 
