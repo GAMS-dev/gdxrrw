cb2d2f56d8a5dcc65d26c52d6cc9e5db3dd59681
sdirkse <sdirkse>
2011-08-01 22:06:59 +0000

    starting checks for rgdx.param

---
+++ sandbox/ppTest.R
@@ -0,0 +1,35 @@
+gdxName <- "eurodist"
+symName <- "dist"
+# df <- rgdx.pp("eurodist","dist")
+df <- rgdx.param(gdxName,symName)
+df3 <- rgdx.param(gdxName,symName,names=c("from","to","distance"))
+
+n <- attr(eurodist,"Size")
+cities <- attr(eurodist,"Labels")
+dm <- as.matrix(eurodist)
+
+k <- 0
+for (i in c(1:n)) {
+  for (j in c(1:n)) {
+    if (dm[i,j] > 0) {
+      k <- k + 1
+      print (paste("checking eurodist:", i, j, cities[i], cities[j], dm[i,j]))
+      print (paste("              df3:", as.numeric(df3$from[k]), as.numeric(df3$to[k]), as.character(df3$from[k]), as.character(df3$to[k]), df3$distance[k]))
+      if (i != as.numeric(df3$from[k])) {
+        stop ("mismatch in i")
+      }
+      if (j != as.numeric(df3$to[k])) {
+        stop ("mismatch in j")
+      }
+      if (cities[i] != as.character(df3$from[k])) {
+        stop ("mismatch in cities[i]")
+      }
+      if (cities[j] != as.character(df3$to[k])) {
+        stop ("mismatch in cities[j]")
+      }
+      if (dm[i,j] != df3$distance[k]) {
+        stop ("mismatch in distance[k]")
+      }
+    }
+  }
+}
