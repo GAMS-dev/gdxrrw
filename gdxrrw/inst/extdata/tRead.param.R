04cc2071168c7e6704b110087f6f334b7b97fb6f
sdirkse <sdirkse>
2011-08-02 17:09:56 +0000

    fix test and output

--- gdxrrw/inst/extdata/tRead.param.R
+++ gdxrrw/inst/extdata/tRead.param.R
@@ -3,19 +3,26 @@ symName <- "dist"
 # df <- rgdx.pp("eurodist","dist")
 df <- rgdx.param(gdxName,symName)
 df3 <- rgdx.param(gdxName,symName,names=c("from","to","distance"))
-dfe <- rgdx.param(gdxName,symName,names=c("from","to","distance"),crosstab="ffff")
+dfe <- rgdx.param(gdxName,symName,names=c("from","to","distance"),crosstab="to")
 
 n <- attr(eurodist,"Size")
 cities <- attr(eurodist,"Labels")
 dm <- as.matrix(eurodist)
 
 k <- 0
+doPrint <- TRUE;
 for (i in c(1:n)) {
   for (j in c(1:n)) {
     if (dm[i,j] > 0) {
       k <- k + 1
-      print (paste("checking eurodist:", i, j, cities[i], cities[j], dm[i,j]))
-      print (paste("              df3:", as.numeric(df3$from[k]), as.numeric(df3$to[k]), as.character(df3$from[k]), as.character(df3$to[k]), df3$distance[k]))
+      if (doPrint) {
+        print (paste("checking eurodist:", i, j, cities[i], cities[j], dm[i,j]))
+        print (paste("              df3:", as.numeric(df3$from[k]), as.numeric(df3$to[k]), as.character(df3$from[k]), as.character(df3$to[k]), df3$distance[k]))
+        if (k > 10) {
+          print ("Further output suppressed")
+          doPrint <- FALSE;
+        }
+      }
       if (i != as.numeric(df3$from[k])) {
         stop ("mismatch in i")
       }
@@ -34,3 +41,5 @@ for (i in c(1:n)) {
     }
   }
 }
+
+print ("All tests for rgdx.param passed");
