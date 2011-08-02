49ef91554fcf58635b67da55f1b73bf8982d44a5
sdirkse <sdirkse>
2011-08-02 22:12:26 +0000

    fix tests

--- gdxrrw/inst/extdata/tRead.param.R
+++ gdxrrw/inst/extdata/tRead.param.R
@@ -1,45 +1,55 @@
-gdxName <- "eurodist"
-symName <- "dist"
-# df <- rgdx.pp("eurodist","dist")
-df <- rgdx.param(gdxName,symName)
-df3 <- rgdx.param(gdxName,symName,names=c("from","to","distance"))
-# dfe <- rgdx.param(gdxName,symName,names=c("from","to","distance"),crosstab="to")
+### Test rgdx.param
+# we read the distance matrix from the eurodist data set
+# previously stored in a GDX file and compare that to the eurodist data
 
-n <- attr(eurodist,"Size")
-cities <- attr(eurodist,"Labels")
-dm <- as.matrix(eurodist)
+tryCatch({
+  gdxName <- "eurodist"
+  symName <- "dist"
+  # df <- rgdx.pp("eurodist","dist")
+  df <- rgdx.param(gdxName,symName)
+  df3 <- rgdx.param(gdxName,symName,names=c("from","to","distance"))
+  # dfe <- rgdx.param(gdxName,symName,names=c("from","to","distance"),crosstab="to")
 
-k <- 0
-doPrint <- TRUE;
-for (i in c(1:n)) {
-  for (j in c(1:n)) {
-    if (dm[i,j] > 0) {
-      k <- k + 1
-      if (doPrint) {
-        print (paste("checking eurodist:", i, j, cities[i], cities[j], dm[i,j]))
-        print (paste("              df3:", as.numeric(df3$from[k]), as.numeric(df3$to[k]), as.character(df3$from[k]), as.character(df3$to[k]), df3$distance[k]))
-        if (k > 10) {
-          print ("Further output suppressed")
-          doPrint <- FALSE;
+  n <- attr(eurodist,"Size")
+  cities <- attr(eurodist,"Labels")
+  dm <- as.matrix(eurodist)
+
+  k <- 0
+  doPrint <- TRUE;
+  for (i in c(1:n)) {
+    for (j in c(1:n)) {
+      if (dm[i,j] > 0) {
+        k <- k + 1
+        if (doPrint) {
+          print (paste("checking eurodist:", i, j, cities[i], cities[j], dm[i,j]))
+          print (paste("              df3:", as.numeric(df3$from[k]), as.numeric(df3$to[k]), as.character(df3$from[k]), as.character(df3$to[k]), df3$distance[k]))
+          if (k > 10) {
+            print ("Further output suppressed")
+            doPrint <- FALSE;
+          }
         }
-      }
-      if (i != as.numeric(df3$from[k])) {
-        stop ("mismatch in i")
-      }
-      if (j != as.numeric(df3$to[k])) {
-        stop ("mismatch in j")
-      }
-      if (cities[i] != as.character(df3$from[k])) {
-        stop ("mismatch in cities[i]")
-      }
-      if (cities[j] != as.character(df3$to[k])) {
-        stop ("mismatch in cities[j]")
-      }
-      if (dm[i,j] != df3$distance[k]) {
-        stop ("mismatch in distance[k]")
+        if (i != as.numeric(df3$from[k])) {
+          stop ("mismatch in i")
+        }
+        if (j != as.numeric(df3$to[k])) {
+          stop ("mismatch in j")
+        }
+        if (cities[i] != as.character(df3$from[k])) {
+          stop ("mismatch in cities[i]")
+        }
+        if (cities[j] != as.character(df3$to[k])) {
+          stop ("mismatch in cities[j]")
+        }
+        if (dm[i,j] != df3$distance[k]) {
+          stop ("mismatch in distance[k]")
+        }
       }
     }
   }
+
+  print ("All tests for rgdx.param passed");
+  return (TRUE)
 }
 
-print ("All tests for rgdx.param passed");
+, error = function(ex) { print(ex) ; return (FALSE) }
+)
--- gdxrrw/inst/extdata/tRead.set.R
+++ gdxrrw/inst/extdata/tRead.set.R
@@ -3,55 +3,60 @@
 # first from GDX and then from .csv files produced by GAMS,
 # and compare the contents to be sure we get the same results
 
-ix <- rgdx.set('ijk','i',names="I")
-jx <- rgdx.set('ijk','j',names="J")
-kx <- rgdx.set('ijk','k',names="K")
+tryCatch({
+  ix <- rgdx.set('ijk','i',names="I")
+  jx <- rgdx.set('ijk','j',names="J")
+  kx <- rgdx.set('ijk','k',names="K")
 
-iv <- read.csv("I.csv")
-jv <- read.csv("J.csv")
-kv <- read.csv("K.csv")
+  iv <- read.csv("I.csv")
+  jv <- read.csv("J.csv")
+  kv <- read.csv("K.csv")
 
-# both rgdx.set and read.csv return a data frame with one factor for
-# each set/column.  We extract the factors and compare them.  Note
-# that we compare the characters in the factors, not the scheme to
-# encode these characters.
+  # both rgdx.set and read.csv return a data frame with one factor for
+  # each set/column.  We extract the factors and compare them.  Note
+  # that we compare the characters in the factors, not the scheme to
+  # encode these characters.
 
-ixf <- ix$I
-jxf <- jx$J
-kxf <- kx$K
-ivf <- iv$I
-jvf <- jv$J
-kvf <- kv$K
-ixfc <- as.character(ixf)
-jxfc <- as.character(jxf)
-kxfc <- as.character(kxf)
-ivfc <- as.character(ivf)
-jvfc <- as.character(jvf)
-kvfc <- as.character(kvf)
+  ixf <- ix$I
+  jxf <- jx$J
+  kxf <- kx$K
+  ivf <- iv$I
+  jvf <- jv$J
+  kvf <- kv$K
+  ixfc <- as.character(ixf)
+  jxfc <- as.character(jxf)
+  kxfc <- as.character(kxf)
+  ivfc <- as.character(ivf)
+  jvfc <- as.character(jvf)
+  kvfc <- as.character(kvf)
 
-if (prod(ixfc != ivfc)) stop ("mismatch in set I")
-if (prod(jxfc != jvfc)) stop ("mismatch in set J")
-if (prod(kxfc != kvfc)) stop ("mismatch in set K")
+  if (prod(ixfc != ivfc)) stop ("mismatch in set I")
+  if (prod(jxfc != jvfc)) stop ("mismatch in set J")
+  if (prod(kxfc != kvfc)) stop ("mismatch in set K")
 
-ijkx <- rgdx.set('ijk','ijk',names=c("I","J","K"))
-ijkv <- read.csv("ijk.csv")
+  ijkx <- rgdx.set('ijk','ijk',names=c("I","J","K"))
+  ijkv <- read.csv("ijk.csv")
 
-ixf <- ijkx$I
-jxf <- ijkx$J
-kxf <- ijkx$K
-ivf <- ijkv$I
-jvf <- ijkv$J
-kvf <- ijkv$K
-ixfc <- as.character(ixf)
-jxfc <- as.character(jxf)
-kxfc <- as.character(kxf)
-ivfc <- as.character(ivf)
-jvfc <- as.character(jvf)
-kvfc <- as.character(kvf)
+  ixf <- ijkx$I
+  jxf <- ijkx$J
+  kxf <- ijkx$K
+  ivf <- ijkv$I
+  jvf <- ijkv$J
+  kvf <- ijkv$K
+  ixfc <- as.character(ixf)
+  jxfc <- as.character(jxf)
+  kxfc <- as.character(kxf)
+  ivfc <- as.character(ivf)
+  jvfc <- as.character(jvf)
+  kvfc <- as.character(kvf)
 
-if (prod(ixfc != ivfc)) stop ("mismatch in set ijk, index I")
-if (prod(jxfc != jvfc)) stop ("mismatch in set ijk, index J")
-if (prod(kxfc != kvfc)) stop ("mismatch in set ijk, index K")
+  if (prod(ixfc != ivfc)) stop ("mismatch in set ijk, index I")
+  if (prod(jxfc != jvfc)) stop ("mismatch in set ijk, index J")
+  if (prod(kxfc != kvfc)) stop ("mismatch in set ijk, index K")
 
+  print ("All tests for rgdx.set passed");
+  return (TRUE)
+}
 
-print ("All tests for rgdx.set passed");
+, error = function(ex) { print(ex) ; return (FALSE) }
+)
