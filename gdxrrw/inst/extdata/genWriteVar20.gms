f679b620b6ea41d058747c58112cb3d4131dc260
sdirkse <sdirkse>
2013-06-11 16:51:11 +0000

    add test of write for 20-dim variable

---
+++ gdxrrw/inst/extdata/genWriteVar20.gms
@@ -0,0 +1,62 @@
+$ontext
+GAMS code to generate GDX "target" for wgdx test of 20-dim variable
+$offtext
+
+sets
+  d1 / d1_1, d1_2 /
+  d2 / d2_1, d2_2 /
+  d3 / d3_1, d3_2 /
+  d4 / d4_1, d4_2 /
+  d5 / d5_1, d5_2 /
+  d6 / d6_1, d6_2 /
+  d7 / d7_1, d7_2 /
+  d8 / d8_1, d8_2 /
+  d9 / d9_1, d9_2 /
+  da / da_1, da_2 /
+  db / db_1, db_2 /
+  dc / dc_1, dc_2 /
+  dd / dd_1, dd_2 /
+  de / de_1, de_2 /
+  df / df_1, df_2 /
+  dg / dg_1, dg_2 /
+  dh / dh_1, dh_2 /
+  di / di_1, di_2 /
+  dj / dj_1, dj_2 /
+  dk / dk_1, dk_2 /
+  ;
+free variable big(d1,d2,d3,d4,d5,d6,d7,d8,d9,da,
+                  db,dc,dd,de,df,dg,dh,di,dj,dk) '20-dim var';
+
+big.L(d1,d2,d3,d4,d5,d6,d7,d8,d9,da,
+      db,dc,dd,de,df,dg,dh,di,dj,dk) = 0.75;
+
+* change in d2
+big.l    ('d1_1','d2_2','d3_1','d4_1','d5_1','d6_1','d7_1','d8_1','d9_1','da_1',
+          'db_1','dc_1','dd_1','de_1','df_1','dg_1','dh_1','di_1','dj_1','dk_1') = 0;
+big.m    ('d1_1','d2_2','d3_1','d4_1','d5_1','d6_1','d7_1','d8_1','d9_1','da_1',
+          'db_1','dc_1','dd_1','de_1','df_1','dg_1','dh_1','di_1','dj_1','dk_1') = 0.5;
+
+* change in d3
+big.l    ('d1_1','d2_1','d3_2','d4_1','d5_1','d6_1','d7_1','d8_1','d9_1','da_1',
+          'db_1','dc_1','dd_1','de_1','df_1','dg_1','dh_1','di_1','dj_1','dk_1') = 0;
+big.lo   ('d1_1','d2_1','d3_2','d4_1','d5_1','d6_1','d7_1','d8_1','d9_1','da_1',
+          'db_1','dc_1','dd_1','de_1','df_1','dg_1','dh_1','di_1','dj_1','dk_1') = 0;
+
+* change in d4
+big.l    ('d1_1','d2_1','d3_1','d4_2','d5_1','d6_1','d7_1','d8_1','d9_1','da_1',
+          'db_1','dc_1','dd_1','de_1','df_1','dg_1','dh_1','di_1','dj_1','dk_1') = 0;
+big.up   ('d1_1','d2_1','d3_1','d4_2','d5_1','d6_1','d7_1','d8_1','d9_1','da_1',
+          'db_1','dc_1','dd_1','de_1','df_1','dg_1','dh_1','di_1','dj_1','dk_1') = 0;
+
+* change in d5
+big.l    ('d1_1','d2_1','d3_1','d4_1','d5_2','d6_1','d7_1','d8_1','d9_1','da_1',
+          'db_1','dc_1','dd_1','de_1','df_1','dg_1','dh_1','di_1','dj_1','dk_1') = 0;
+big.scale('d1_1','d2_1','d3_1','d4_1','d5_2','d6_1','d7_1','d8_1','d9_1','da_1',
+          'db_1','dc_1','dd_1','de_1','df_1','dg_1','dh_1','di_1','dj_1','dk_1') = 100;
+
+execute_unload 'tVar20';
+
+scalar c;
+c = card(big);
+file log / '' /;
+putclose log / 'Card(big) = ', c /;
---
+++ gdxrrw/inst/extdata/tWriteVar20.R
@@ -0,0 +1,114 @@
+## ## test wgdx with form='sparse', 20-d variable: see if we can go big
+
+if (! require(gdxrrw))      stop ("gdxrrw package is not available")
+if (0 == igdx(silent=TRUE)) stop ("the gdx shared library has not been loaded")
+
+testName <- '20-dim var with 2**20 ~= 1M entries'
+
+errFunc <- function(ex) {
+  print (paste("test of wgdx on",testName,"failed"))
+  print(ex)
+  FALSE
+} # errFunc
+
+tryCatch({
+  print (paste("testing wgdx on", testName))
+  wgdx('?')
+  fnOut <- "tmp.gdx"
+  fnWant <- "tVar20.gdx"
+  if (! file_test ('-f', fnWant)) {
+    stop (paste("FAIL: File-to-duplicate", fnWant, "does not exist"))
+  }
+
+  u1 <- c("d1_1","d1_2")
+  u2 <- c("d2_1","d2_2")
+  u3 <- c("d3_1","d3_2")
+  u4 <- c("d4_1","d4_2")
+  u5 <- c("d5_1","d5_2")
+  u6 <- c("d6_1","d6_2")
+  u7 <- c("d7_1","d7_2")
+  u8 <- c("d8_1","d8_2")
+  u9 <- c("d9_1","d9_2")
+  ua <- c("da_1","da_2")
+  ub <- c("db_1","db_2")
+  uc <- c("dc_1","dc_2")
+  ud <- c("dd_1","dd_2")
+  ue <- c("de_1","de_2")
+  uf <- c("df_1","df_2")
+  ug <- c("dg_1","dg_2")
+  uh <- c("dh_1","dh_2")
+  ui <- c("di_1","di_2")
+  uj <- c("dj_1","dj_2")
+  uk <- c("dk_1","dk_2")
+  fUels <- c("l", "m", "lo", "up", "s")
+  uels <- list(u1,u2,u3,u4,u5,u6,u7,u8,u9,ua,
+               ub,uc,ud,ue,uf,ug,uh,ui,uj,uk,fUels)
+#  uels <- list(u1,u2,u3,u4,fUels)
+
+  nd <- 20                              # number of dimensions
+  ncol <- nd + 2
+  nrow <- 2**nd
+  step <- nrow / 2
+  v <- matrix(0,nrow=nrow,ncol=ncol)
+  for (d in 1:nd) {
+    print(paste("d = ",d))
+    b <- 0
+    while (b < nrow) {
+      # print(paste("  assigning 1 to",b+(1:step)))
+      v[b+1:step,d] <- 1
+      b <- b + step
+      # print(paste("  assigning 2 to",b+(1:step)))
+      v[b+1:step,d] <- 2
+      b <- b + step
+    }
+    step <- step / 2
+  }
+  v[1:nrow,nd+1] <- 1                  # field=level
+  v[1:nrow,ncol] <- 0.75
+
+  ## patch in a marginal: idx = (1,2,1,1,1,***)
+  b <- nrow / 4
+  irow <- b + 1
+  v[irow,nd+1] <- 2                     # field=marginal
+  v[irow,ncol] <- 0.5
+
+  ## patch in a lower: idx = (1,1,2,1,1,1,***)
+  b <- b / 2
+  irow <- b + 1
+  v[irow,nd+1] <- 3                     # field=lower
+  v[irow,ncol] <- 0
+
+  ## patch in an upper: idx = (1,1,1,2,1,1,1,***)
+  b <- b / 2
+  irow <- b + 1
+  v[irow,nd+1] <- 4                     # field=upper
+  v[irow,ncol] <- 0
+
+  ## patch in a scale: idx = (1,1,1,1,2,1,1,1,***)
+  b <- b / 2
+  irow <- b + 1
+  v[irow,nd+1] <- 5                     # field=scale
+  v[irow,ncol] <- 100
+
+  vList <- list(name='big',type='variable',val=v,uels=uels,
+                  typeCode=GMS_VARTYPE$FREE,ts='20-dim var')
+  wgdx (fnOut, vList)
+  if (file_test ('-f', fnOut) == TRUE) {
+    print (paste("File", fnOut, "was created"))
+  } else {
+    stop (paste("FAIL: File", fnOut, "is not readable"))
+  }
+  rc <- system (paste("gdxdiff", fnWant, fnOut, "id=big"))
+  if (0 != rc) {
+    stop(paste("Bad return from gdxdiff: wanted 0, got",rc))
+  } else {
+    print ("gdxdiff call succeeded")
+  }
+
+
+  print (paste("test of wgdx on", testName, "passed"))
+  TRUE   ## all tests passed: return TRUE
+},
+
+error = errFunc
+)
