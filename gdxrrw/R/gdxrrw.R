4b8fe1866c9f29c8f1b3dc786c1653801df30e64
sdirkse <sdirkse>
2013-07-09 18:59:01 +0000

    updates and tests for wgdx.lst

--- gdxrrw/R/gdxrrw.R
+++ gdxrrw/R/gdxrrw.R
@@ -217,6 +217,69 @@ rgdx.set <- function(gdxName, symName, names=NULL, compress=FALSE, ts=FALSE, use
   return(symDF)
 } # rgdx.set
 
+## processScalar: process a scalar given as input to wgdx
+## output a list suitable for raw wgdx
+processScalar <- function(s, msg)
+{
+  symName <- attr(s, "symName", exact=TRUE)
+  if (! is.character(symName)) {
+    stop ("error processing ", msg, ": missing/bogus symName attribute")
+  }
+  o <- list (name=symName, type="parameter", dim=0, form="full", val=as.numeric(s))
+  symText <- attr(s, "ts", exact=TRUE)
+  if (is.character(symText)) {
+    o$ts <- symText
+  }
+  o
+} # processScalar
+
+## processDF: process a dataframe given as input to wgdx
+## output a list suitable for raw wgdx
+processDF <- function(df, msg)
+{
+  symName <- attr(df, "symName", exact=TRUE)
+  if (! is.character(symName)) {
+    stop ("error processing ", msg, ": missing/bogus symName attribute")
+  }
+  nr <- nrow(df)
+  nc <- ncol(df)
+  isSet <- TRUE
+  if (! is.factor(df[[1]])) {
+    stop ("(",msg,")[[1]] must be a factor")
+  }
+  for (j in 1 + seq_len(max(0,nc-2))) {
+    if (! is.factor(df[[j]])) {
+      stop ("(",msg,")[[", j, "]] must be a factor")
+    }
+  }
+  if (is.factor(df[[nc]])) {
+    symType <- "set"
+    symDim <- nc
+  }
+  else if (is.numeric(df[[nc]])) {
+    symType <- "parameter"
+    isSet <- FALSE
+    symDim <- nc-1
+  }
+  o <- list (name=symName, type=symType, dim=symDim, form="sparse")
+  symText <- attr(df, "ts", exact=TRUE)
+  if (is.character(symText)) {
+    o$ts <- symText
+  }
+  v <- matrix(0, nrow=nr, ncol=nc)
+  uels <- c()
+  if (! isSet) {
+    v[,symDim+1] <- df[,symDim+1]
+  }
+  for (j in c(1:symDim)) {
+    v[,j] <- as.numeric(df[,j])
+    uels <- c(uels,list(levels(df[,j])))
+  }
+  o$val <- v
+  o$uels <- uels
+  o
+} # processDF
+
 wgdx.df <- function(gdxName, df, squeeze='y')
 {
   if (! is.character(gdxName)) {
@@ -291,79 +354,86 @@ wgdx.scalar <- function(gdxName, s)
   wgdx (gdxName, lst)
 } # wgdx.scalar
 
-# input list of symbols: contents are data frames, scalars, or symLists
-wgdx.lst <- function(gdxName, ilst, squeeze='y')
+# wgdx.lst: write multiple symbols to a GDX file
+# the routines above write only one symbol to GDX, where the symbol info can
+# take different forms:
+#   wgdx.df    : input is a data frame containing a set or param
+#   wgdx.scalar: input is a scalar
+#   wgdx       : input is one or more lists, each list holds one symbol
+# This routine accepts all three types of input, singly or in lists
+# Let S be a data frame, scalar, or list-holding-a-symbol, as just mentioned
+# Let L be a list of elements of type S.
+# Each element of "..." for this function must be of type L or type S
+wgdx.lst <- function(gdxName, ..., squeeze='y')
 {
   if (! is.character(gdxName)) {
-    stop ("gdxName must GDX file name")
+    stop ("bad gdxName: must be a GDX file name")
   }
-  if (! is.list(ilst)) {
-    stop ("ilst must be a list of inputs")
-  }
+  dotList <- list(...)
+  nInputs <- length(dotList)
   olst <- list()
-  inLen <- length(ilst)
-  for (i in c(1:inLen)) {
-    if (is.data.frame(ilst[[i]])) {
-      symName <- attr(ilst[[i]], "symName", exact=TRUE)
-      if (! is.character(symName)) {
-        stop ("error processing ilst[[",i,"]]: missing/bogus symName attribute")
-      }
-      nr <- nrow(ilst[[i]])
-      nc <- ncol(ilst[[i]])
-      isSet <- TRUE
-      if (! is.factor(ilst[[i]][[1]])) {
-        stop ("ilst[[",i,"]][[1]] must be a factor")
-      }
-      for (j in 1 + seq_len(max(0,nc-2))) {
-        if (! is.factor(ilst[[i]][[j]])) {
-          stop ("ilst[[",i,"]][[", j, "]] must be a factor")
-        }
-      }
-      if (is.factor(ilst[[i]][[nc]])) {
-        symType <- "set"
-        symDim <- nc
-      }
-      else if (is.numeric(ilst[[i]][[nc]])) {
-        symType <- "parameter"
-        isSet <- FALSE
-        symDim <- nc-1
-      }
-      olst[[i]] <- list (name=symName, type=symType, dim=symDim, form="sparse")
-      symText <- attr(ilst[[i]], "ts", exact=TRUE)
-      if (is.character(symText)) {
-        olst[[i]]$ts <- symText
-      }
-      v <- matrix(0, nrow=nr, ncol=nc)
-      uels <- c()
-      if (! isSet) {
-        v[,symDim+1] <- ilst[[i]][,symDim+1]
-      }
-      for (j in c(1:symDim)) {
-        v[,j] <- as.numeric(ilst[[i]][,j])
-        uels <- c(uels,list(levels(ilst[[i]][,j])))
-      }
-      olst[[i]]$val <- v
-      olst[[i]]$uels <- uels
+  kOut <- 0
+
+  for (k in c(1:nInputs)) {
+    item <- dotList[[k]]
+    print (paste("*** Processing item", k))
+    if (is.data.frame(item)) {
+      print (" *** found a data frame")
+      kOut <- kOut + 1
+      olst[[kOut]] <- processDF (item, paste("argument",k+1))
     }
-    else if (! is.list (ilst[[i]]) &&
-             is.numeric(ilst[[i]]) &&
-             is.null(dim(ilst[[i]])) ) {
+    else if (! is.list (item) &&
+             is.numeric(item) &&
+             is.null(dim(item)) ) {
       # reading a scalar
-      symName <- attr(ilst[[i]], "symName", exact=TRUE)
-      if (! is.character(symName)) {
-        stop ("error processing ilst[[",i,"]]: missing/bogus symName attribute")
-      }
-      olst[[i]] <- list (name=symName, type="parameter", dim=0, form="full", val=as.numeric(ilst[[i]]))
-      symText <- attr(ilst[[i]], "ts", exact=TRUE)
-      if (is.character(symText)) {
-        olst[[i]]$ts <- symText
-      }
+      print (" *** found a scalar")
+      kOut <- kOut + 1
+      olst[[kOut]] <- processScalar (item, paste("argument",k+1))
     }
-    else if (is.list (ilst[[i]])) {
-      olst[[i]] <- ilst[[i]]
+    else if (is.list (item)) {
+      if (is.null(names(item))) {
+        ## unnamed list: each element must be a dataframe, scalar, or symbol list
+        print (" *** found an unnamed list:")
+        nList <- length(item)
+        for (kk in c(1:nList)) {
+          item2 <- item[[kk]]
+          print (paste("  *** Processing item2", kk))
+          if (is.data.frame(item2)) {
+            print ("   *** found a data frame")
+            kOut <- kOut + 1
+            olst[[kOut]] <- processDF (item2, paste0("arg",k+1,"[[",kk,"]]"))
+          }
+          else if (! is.list (item2) &&
+                   is.numeric(item2) &&
+                   is.null(dim(item2)) ) {
+                                        # reading a scalar
+            print ("   *** found a scalar")
+            kOut <- kOut + 1
+            olst[[kOut]] <- processScalar (item2, paste0("arg",k+1,"[[",kk,"]]"))
+          }
+          else if (is.list (item2)) {
+            if (! is.null(names(item2))) {
+              ## named list: add to the outputs
+              kOut <- kOut + 1
+              olst[[kOut]] <- item2
+            }
+            else {
+              stop (paste0("invalid input found: arg",k+1,"[[",kk,"]] is a list but does not contain symbol info"))
+            }
+          }
+          else {
+            stop (paste0("unrecognized input found: arg",k+1,"[[",kk,"]] not valid"))
+          }
+        } # loop over kk: item2 members of item list
+      }
+      else {
+        print (" *** found a named list:")
+        kOut <- kOut + 1
+        olst[[kOut]] <- item
+      }
     }
     else {
-      stop ("unrecognized input found: ilst[[",i,"]]")
+      stop (paste("unrecognized input found: argument",k+1,"not valid"))
     }
   }
 
