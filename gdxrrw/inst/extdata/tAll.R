3916482f5ffcee275405d9b6aec43c89b8c85628
sdirkse <sdirkse>
2013-07-31 15:36:55 +0000

    implement quick mode for tests

--- gdxrrw/inst/extdata/tAll.R
+++ gdxrrw/inst/extdata/tAll.R
@@ -12,6 +12,18 @@ Sys.setenv(PATH=paste(gdir,pth,sep=sepChar))
 rc <- source ("tgdxdiff.R")
 if (! rc$value)  stop ("The gdxdiff utility is unavailable or broken: Test run aborted.")
 
+if (! interactive()) {
+  a <- commandArgs(trailingOnly=TRUE)
+  quickMode <- 0
+  if ((length(a) > 0) && ("quick" == a[1]))  quickMode <- 1
+  print(paste("quick set via commandArgs =",quickMode))
+} else {
+  f <- find('quickMode',mode='numeric')
+  if (0 == length(f)) quickMode <- 0
+  print(paste("quick set interactively =",quickMode))
+}
+
+# first the majority of tests: these are quicker
 tests <- c("tReadSparse1", "tReadFull1",
            "tRead.set", "tRead.param",
            "tReadSV", "tReadText", "tReadCase",
@@ -25,11 +37,14 @@ tests <- c("tReadSparse1", "tReadFull1",
            "tWriteSV",
            "tWriteEquTypes",
            "tWriteVarField", "tWriteVarSort", "tWriteVarOrder",
-           "tWriteVarTypesSparse", "tWriteVar20", "tWriteVarFUels",
+           "tWriteVarTypesSparse", "tWriteVarFUels",
            "tWriteVarTypesFull",
            "tWrap",
            "tInfo1", "tInfo2",
            "tLS")
+if (0 == quickMode) {
+  tests <- c(tests, "tWriteVar20")
+}
 f <- find('interact',mode='logical')
 doPrompt <- FALSE
 if (length(f) > 0) {
@@ -60,6 +75,7 @@ for (ttt in tests) {
   }
 }
 Sys.setenv(PATH=pth)
+if (quickMode) print ("N.B.: quickMode is on, some tests were skipped")
 if (nFails) {
   print (paste("Tests complete. ", nRuns,"tests run,",nFails,"tests FAILED"))
   for (nm in names(fTests)) print(paste0('  ',nm))
