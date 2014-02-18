ed0bf27da61dc17148804bff586ccb4eb3a920f8
sdirkse <sdirkse>
2014-02-18 19:38:07 +0000

    add tests for set text writing

---
+++ gdxrrw/inst/extdata/tWriteSetText.R
@@ -0,0 +1,121 @@
+## test wgdx on writing set text
+## gdxdump or gdxdiff do not really do what we want here, so just have
+## to read from the generated GDX and the target GDX and compare the results
+
+if (! require(gdxrrw))      stop ("gdxrrw package is not available")
+if (0 == igdx(silent=TRUE)) stop ("the gdx shared library has not been loaded")
+
+testName <- 'writing set text'
+
+errFunc <- function(ex) {
+  print (paste0("test of wgdx on ",testName,": FAILED"))
+  print(ex)
+  FALSE
+} # errFunc
+
+tryCatch({
+  print (paste("testing wgdx on", testName))
+  wgdx('?')
+  fnOut <- "tmp.gdx"
+  fnWant <- "teWriteSetText.gdx"
+  if (! file_test ('-f', fnWant)) {
+    stop (paste("FAIL: File-to-duplicate", fnWant, "does not exist"))
+  }
+
+  iUels <- paste0("i","1":"4")
+  iN <- length(iUels)
+  jUels <- paste0("j","1":"3")
+  jN <- length(jUels)
+  ijN <- 5
+  uels <- list(c(iUels,jUels))
+  valI <- matrix(0,nrow=iN,ncol=1)
+  for (k in 1:iN) {
+    valI[k,1] <- k
+  }
+  teI <- matrix(NA_character_,nrow=iN,ncol=1)
+  teI[1,1] <- "i1 associated text"
+  teI[2,1] <- "i2's text here"
+  teI[3,1] <- "  "
+
+  valJ <- matrix(0,nrow=jN,ncol=1)
+  for (k in 1:jN) {
+    valJ[k,1] <- iN + k
+  }
+  teJ <- matrix(NA_character_,nrow=jN,ncol=1)
+  teJ[1,1] <- "j1 text"
+  teJ[2,1] <- "j2 text"
+  teJ[3,1] <- "j3 text"
+
+  valIJ <- matrix(c(1,1,
+                    2,1,
+                    2,2,
+                    3,3,
+                    4,3),
+                  nrow=ijN,ncol=2,byrow=TRUE)
+  teIJ <- matrix(NA_character_,nrow=ijN,ncol=1)
+  teIJ[1,1] <- "one.one"
+  teIJ[3,1] <- "trailing blank "
+  teIJ[4,1] <- "three.three"
+  teIJ[5,1] <- " "
+
+  vI <- list(name='I',type='set',form='sparse',val=valI,uels=uels,
+             ts='',te=teI)
+  vJ <- list(name='J',type='set',form='sparse',val=valJ,uels=uels,
+             ts='',te=teJ)
+  vIJ <- list(name='IJ',type='set',form='sparse',val=valIJ,uels=list(iUels,jUels),
+              ts='',te=teIJ)
+
+  ## test with inventSetText at default
+  options(gdx.inventSetText=NULL)
+  wgdx.lst(fnOut, vI, vJ, vIJ)
+  if (file_test ('-f', fnOut) == TRUE) {
+    print (paste("File", fnOut, "was created"))
+  } else {
+    stop (paste("FAIL: File", fnOut, "is not readable"))
+  }
+  rc <- system (paste("gdxdiff", fnWant, fnOut))
+  if (0 != rc) {
+    stop(paste("Bad return from gdxdiff: wanted 0, got",rc))
+  } else {
+#    print ("gdxdiff call succeeded")
+  }
+
+  ## test with inventSetText=NA
+  options(gdx.inventSetText=NA)
+  wgdx.lst(fnOut, vI, vJ, vIJ)
+  if (file_test ('-f', fnOut) == TRUE) {
+    print (paste("File", fnOut, "was created"))
+  } else {
+    stop (paste("FAIL: File", fnOut, "is not readable"))
+  }
+  rc <- system (paste("gdxdiff", fnWant, fnOut))
+  if (0 != rc) {
+    stop(paste("Bad return from gdxdiff: wanted 0, got",rc))
+  } else {
+#    print ("gdxdiff call succeeded")
+  }
+
+    ## test with inventSetText=FALSE
+  options(gdx.inventSetText=F)
+  wgdx.lst(fnOut, vI, vJ, vIJ)
+  if (file_test ('-f', fnOut) == TRUE) {
+    print (paste("File", fnOut, "was created"))
+  } else {
+    stop (paste("FAIL: File", fnOut, "is not readable"))
+  }
+  rc <- system (paste("gdxdiff", fnWant, fnOut))
+  if (0 != rc) {
+    stop(paste("Bad return from gdxdiff: wanted 0, got",rc))
+  } else {
+#    print ("gdxdiff call succeeded")
+  }
+
+  ## test with inventSetText=TRUE
+  options(gdx.inventSetText=T)
+
+  print (paste0("test of wgdx on ", testName, ": PASSED"))
+  invisible(TRUE)   ## all tests passed: return TRUE
+},
+
+error = errFunc
+)
