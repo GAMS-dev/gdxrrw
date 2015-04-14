e4033898fa7fdc17184033826e1c4ba3dc868b7c
sdirkse <sdirkse>
2015-04-14 01:34:41 +0000

    add domInfo as an expected field when comparing

--- gdxrrw/inst/tests/chkSame.R
+++ gdxrrw/inst/tests/chkSame.R
@@ -375,6 +375,11 @@ chkRgdxRes <- function(f1, f2, checkDimNames=FALSE, reqIdent=FALSE) {
       if (! is.character(f2[[k]]))   return (r)
       if (f1[[k]] != f2[[k]])        return (r)
     }
+    else if ("domInfo" == f2Names[[k]]) {
+      if (! is.character(f1[[k]]))   return (r)
+      if (! is.character(f2[[k]]))   return (r)
+      if (f1[[k]] != f2[[k]])        return (r)
+    }
     else if ("uels" == f2Names[[k]]) {
       if (isUniverse) {
         r$msg <- "universe set: expect vector of uels"
