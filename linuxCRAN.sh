9c2b3caf234ac1c6d72c0121f7776d10b16f6247
sdirkse <sdirkse>
2015-05-28 20:16:48 +0000

    prepping for CRAN

--- linuxCRAN.sh
+++ linuxCRAN.sh
@@ -2,3 +2,10 @@
 
 cmd="R CMD build gdxrrw"
 eval $cmd || { rc=$? ; echo "failed cmd: $cmd" ; echo "rc=$rc" ; exit ; }
+
+src=`ls -1 gdxrrw_?.?.?.tar.gz | tail -1`
+
+echo "Just built $src"
+
+cmd="R CMD check $src --as-cran"
+eval $cmd || { rc=$? ; echo "failed cmd: $cmd" ; echo "rc=$rc" ; exit ; }
