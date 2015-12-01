6623fe73f66e69315c8c9dca8c221337c1a2ec94
sdirkse <sdirkse>
2015-12-01 14:36:33 +0000

    fix build script

--- linuxCRAN.sh
+++ linuxCRAN.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-xtra=`svn st --no-ignore | wc -l`
-[ "0" = "$xtra" ] || { echo "unversioned files found!!" ; exit ; }
+xtra=`svn st gdxrrw --no-ignore | wc -l`
+if [ "0" != "$xtra" ] ; then
+    echo "unversioned files found!!"
+    svn st gdxrrw --no-ignore
+    exit 1
+fi
 
 cmd="R CMD build gdxrrw"
-eval $cmd || { rc=$? ; echo "failed cmd: $cmd" ; echo "rc=$rc" ; exit ; }
+eval $cmd || { rc=$? ; echo "failed cmd: $cmd" ; echo "rc=$rc" ; exit 1 ; }
 
 src=`ls -1 gdxrrw_?.?.?.tar.gz | tail -1`
 
 echo "Just built $src"
 
 cmd="R CMD check $src --as-cran"
-eval $cmd || { rc=$? ; echo "failed cmd: $cmd" ; echo "rc=$rc" ; exit ; }
+eval $cmd || { rc=$? ; echo "failed cmd: $cmd" ; echo "rc=$rc" ; exit 1 ; }
