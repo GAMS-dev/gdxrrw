47e79a62b77d1a2b78b24866d8a6f5202d11cbd2
sdirkse <sdirkse>
2019-09-13 14:18:52 +0000

    sync with latest gclgms from master repo

--- gdxrrw/src/gclgms.c
+++ gdxrrw/src/gclgms.c
@@ -5,15 +5,50 @@
 
 #include "gclgms.h"
 
-#if ! defined(_GCL_RHACK_)
-# error "this file modified to work with R extensions.  Do not use outside of R"
-#endif
-
 const char *gmsGdxTypeText[GMS_DT_MAX] =
   {"Set","Parameter","Variable","Equation","Alias"};
-const char *gmsVarTypeText[GMS_VARTYPE_MAX] = {"unknown","binary","integer","positive","negative","free","sos1","sos2","semicont","semiint"};
+const char *gmsVarTypeText[GMS_VARTYPE_MAX] = {"Unknown","Binary","Integer","Positive","Negative","Free","Sos1","Sos2","Semicont","Semiint"};
 const char *gmsValTypeText[GMS_VAL_MAX] = {".l",".m",".lo",".up",".scale"};
 const char *gmsSVText[GMS_SVIDX_MAX] = {"UNdef","NA","+Inf","-Inf","Eps","0","AcroN"};
+
+const char * const rcStat[STAT_MAX] = {"     "," NOPT","INFES","UNBND"," ****","UNKNW","REDEF","DEPND","REDIR"};
+
+const char * const solveStatusTxt[SS_MAX] = {"NA                      ",
+                                             "Normal Completion       ",
+                                             "Iteration Interrupt     ",
+                                             "Resource Interrupt      ",
+                                             "Terminated By Solver    ",
+                                             "Evaluation Interrupt    ",
+                                             "Capability Problems     ",
+                                             "Licensing Problems      ",
+                                             "User Interrupt          ",
+                                             "Setup Failure           ",
+                                             "Solver Failure          ",
+                                             "Internal Solver Failure ",
+                                             "Solve Processing Skipped",
+                                             "System Failure          "};
+
+const char * const modelStatusTxt[MS_MAX] = {"NA                      ",
+                                             "Optimal                 ",
+                                             "Locally Optimal         ",
+                                             "Unbounded               ",
+                                             "Infeasible              ",
+                                             "Locally Infeasible      ",
+                                             "Intermediate Infeasible ",
+                                             "Feasible Solution       ",
+                                             "Integer Solution        ",
+                                             "Intermediate Non-Integer",
+                                             "Integer Infeasible      ",
+                                             "Licensing Problem       ",
+                                             "Error Unknown           ",
+                                             "Error No Solution       ",
+                                             "No Solution Returned    ",
+                                             "Solved Unique           ",
+                                             "Solved                  ",
+                                             "Solved Singular         ",
+                                             "Unbounded - No Solution ",
+                                             "Infeasible - No Solution"};
+
 
 const double gmsDefRecVar[GMS_VARTYPE_MAX][GMS_VAL_MAX] = {
  /* .l   .m           .lo         .ub  .scale */
@@ -36,7 +71,8 @@ const double gmsDefRecEqu[GMS_EQUTYPE_MAX][GMS_VAL_MAX] = {
   { 0.0, 0.0, GMS_SV_MINF,         0.0, 1.0},    /* =l= */
   { 0.0, 0.0, GMS_SV_MINF, GMS_SV_PINF, 1.0},    /* =n= */
   { 0.0, 0.0,         0.0,         0.0, 1.0},    /* =x= */
-  { 0.0, 0.0,         0.0, GMS_SV_PINF, 1.0}     /* =c= */
+  { 0.0, 0.0,         0.0, GMS_SV_PINF, 1.0},    /* =c= */
+  { 0.0, 0.0,         0.0,         0.0, 1.0}     /* =b= */
 };
 
 /* extract an equation type in [GMS_EQUTYPE_E,GMS_EQUTYPE_MAX)
