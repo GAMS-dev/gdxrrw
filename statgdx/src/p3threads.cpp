ef377a9655cd5292df7e04060d9333fe173b18c7
Steven Dirkse <sdirkse@gams.com>
2021-01-26 13:00:29 -0500

    decruft

--- statgdx/src/p3threads.cpp
+++
@@ -1,9 +0,0 @@
-/* unit p3threads */
-void _Init_Module_p3threads(void)
-{
-} /* _Init_Module_p3threads */
-
-void _Final_Module_p3threads(void)
-{
-} /* _Final_Module_p3threads */
-
--- unusedSrc4StatgdxDebug/ddd.h
+++
@@ -1,8 +0,0 @@
-#if defined(__cplusplus)
-extern "C" {
-  void xxxfree (SYSTEM_pointer *pgdx);
-}
-#else
-void xxxfree (SYSTEM_pointer *pgdx);
-#endif
-
