ff7c57086e1da279a71fe30a141c1075eb67102d
Steven Dirkse <sdirkse@gams.com>
2021-01-29 20:07:35 +0000

    Resolve "Agree on license text for headers"

--- gdxrrw/src/gdxInfo.c
+++ gdxrrw/src/gdxInfo.c
@@ -1,6 +1,19 @@
 /* gdxInfo.c
  * code for gdxrrw::gdxInfo
- * $Id$
+ * 
+ * Copyright (c) 2010-2021 GAMS Development Corp. <support@gams.com>
+ * Copyright (c) 2010-2021 GAMS Software GmbH <support@gams.com>
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Public License 2.0 which is
+ * available at  http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following
+ * Secondary Licenses when the conditions for such availability set
+ * forth in the Eclipse Public License, v. 2.0 are satisfied:
+ * GNU General Public License, version 2 or later
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
  */
 
 #include <R.h>
--- gdxrrw/src/gdxrrw.c
+++ gdxrrw/src/gdxrrw.c
@@ -1,3 +1,21 @@
+/* gdxrrw.c
+ * code for gdxrrw::gams and gdxrrw::igdx
+ *
+ * Copyright (c) 2010-2021 GAMS Development Corp. <support@gams.com>
+ * Copyright (c) 2010-2021 GAMS Software GmbH <support@gams.com>
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Public License 2.0 which is
+ * available at  http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following
+ * Secondary Licenses when the conditions for such availability set
+ * forth in the Eclipse Public License, v. 2.0 are satisfied:
+ * GNU General Public License, version 2 or later
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+ */
+
 /* This gdxrrw.c file contains methods to import and export data
    between GAMS and R via GDX file. Methods that are exposed for
    the users of R are:
